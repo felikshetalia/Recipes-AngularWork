2daf5ea90dbdcd2285d81d38b913e42ea19c81e8
felikshetalia <mccorsica@yahoo.com>
2025-07-01 16:29:54 +0200

    1. navigation bar and working dialog complete

---
+++ src/app/shared/author-dialog/author-dialog.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { Navbar } from '../../navbar/navbar';
+
+@Component({
+  selector: 'app-author-dialog',
+  imports: [MatDialogModule],
+  standalone: true,
+  templateUrl: './author-dialog.html',
+  styleUrl: './author-dialog.scss'
+})
+export class AuthorDialog {
+  // @Output() close = new EventEmitter<void>();
+  closeDialog() {
+    // this.close.emit();
+    const dialog = inject(MatDialog);
+    dialog.closeAll();
+  }
+}
